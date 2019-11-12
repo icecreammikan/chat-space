47db0afc1f4ebde7410e21e0e6e8e717c8209185
icecreammikan <ryrh31223458@gmail.com>
2019-11-12 14:13:17 +0900

    pullrequest

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
   has_many :messages
   has_many :users,  through:  :users_group
-  has_many :users_groups
+  has_many :users_group
   validates :name, presence: true, uniqueness: true
 end
